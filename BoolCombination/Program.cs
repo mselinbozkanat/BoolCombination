38b4a037f09cec6d1de304fa8a134744a6e6e509
mselinbozkanat <48119991+mselinbozkanat@users.noreply.github.com>
2019-03-05 00:47:08 +0300

    First

---
+++ BoolCombination/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolCombination
+{
+    class Program
+    {
+        public static string d = "t";
+
+        public static string y = "f";
+
+        public static int adet;
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kaç adet True, False için tüm kombinasyonları görmek istiyorsunuz?");
+            adet = Convert.ToInt32(Console.ReadLine());
+            if (adet != 0)
+                BoolCombination1(adet, "");
+
+            Console.ReadKey();
+
+        }
+
+        static void BoolCombination1(int n, string a)
+        {
+
+            if (n > 0)
+            {
+                BoolCombination1(n - 1, a + "t");
+                BoolCombination1(n - 1, a + "f");
+                return;
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
